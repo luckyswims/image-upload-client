4b717b68531ccf5dcacb3aeffe6080758b21e804
John Lucker <john.lucker@comcast.net>
2020-05-10 17:14:35 -0400

    Updated api link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-hamlet-25054.herokuapp.com',
+  production: 'https://luckyswims-image-upload-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
